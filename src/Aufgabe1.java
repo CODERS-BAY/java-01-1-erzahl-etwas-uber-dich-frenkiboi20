c41882516401f5e781a0561869ffeaa68eccdabf
Jakob Penninger <jakob.penninger@gmx.net>
2020-08-27 09:34:27 +0200

    Add main class

---
+++ src/Aufgabe1.java
@@ -0,0 +1,20 @@
+public class Aufgabe1 {
+
+    public static void main(String[] args) {
+
+        byte age = 41;
+        String surname = "Jakob";
+        char gender = 'm';
+        String name = "Penninger";
+        String bornDate = "24.08.1979";
+        float average = 1.3f;
+        boolean isMarried = true;
+
+        System.out.println("Alter: " + age);
+        System.out.println("Vorname: " + surname);
+        System.out.println("Geschlecht: " + gender);
+        System.out.println("Geboren am: " + bornDate);
+        System.out.println("Notendurchschnitt: " + average);
+        System.out.println("Verheiratet: " + isMarried);
+    }
+}
